d51996f1515449058f31de8401b43162fc6663bb
Administrator <admin@example.com>
2017-06-23 12:20:15 +0900

    Initial Setting

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,13 +1,9 @@
 
 public class Calculator {
-	
+
 	public static void main(String[] args) {
-		
-		// print numbers from 0 to 9
-		for(int i=0; i<10; i++) {
-			System.out.println(i);
-		}
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }
