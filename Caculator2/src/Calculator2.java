5718bc678bbbb13db9563d9d41d82c06b73ed7d5
Administrator <admin@example.com>
2017-06-30 11:35:02 +0900

    class name change

---
+++ Caculator2/src/Calculator2.java
@@ -0,0 +1,29 @@
+
+public class Calculator2 {
+
+	public static void main(String[] args) {
+		// 주석 추가
+		// add2
+		// add3
+		// add4
+	}
+	
+	public int add(int num1, int num2) {
+		return num1+num2;	// fix error version at home
+	}
+	
+	public int subtraction(int num1, int num2) {
+		return num1-num2;	// fix error version at home
+	}
+	
+	public int multiplication(int num1, int num2) {
+		return num1*num2;	// fix error version at home
+	}
+	
+	public int division(int num1, int num2) {
+		return num1/num2;	// fix error version at home
+	}
+	
+	// 계산기 구현 완료
+
+}
